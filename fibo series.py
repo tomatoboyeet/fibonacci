f17ea3dae0f4271e990dff1f6c51a7128354c8aa
tomatoboyeet <93995829+tomatoboyeet@users.noreply.github.com>
2021-11-10 15:24:13 +0200

    Update fibo series.py

--- fibo series.py
+++ fibo series.py
@@ -1,12 +1,6 @@
-x,y=1,1
-#we define the two start numbers
-while True:
-#loop forever
-    x=x+y
-#we set x to new fibonacci value
-    print(x)
-#we print x
-    y=x+y
-#we set y to new value
-    print(y)
-#we print y
+x,y=1,1#we define the two start numbers
+while True:#loop forever
+    x=x+y#we set x to new fibonacci value
+    print(x)#we print x
+    y=x+y#we set y to new value
+    print(y)#we print y
